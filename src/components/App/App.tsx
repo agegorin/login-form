b2ac87c83931b0549d8a236e581fccbd8e25f5d6
Egor Konovalov <agegorin@gmail.com>
2022-11-15 11:49:36 +0500

    add modal component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import Background from "../Background/Background";
+import LoginForm from "../LoginForm/LoginForm";
 
 const App = () => {
   return <>
     <Background/>
+    <LoginForm />
   </>;
 }
 
---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Modal from "../Modal/Modal";
+
+
+
+const LoginForm = () => {
+  return <Modal>
+    Login form
+  </Modal>;
+}
+
+export default LoginForm;
